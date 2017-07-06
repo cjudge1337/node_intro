a79f5124ad72cae29c74e6c598f2429eddfe3a94
Carson Judge <cjudge1337@gmail.com>
2017-07-06 15:33:27 -0700

    Finish time server

--- program.js
+++ program.js
@@ -1,33 +1,91 @@
-const http = require('http');
+const net = require('net');
+const server = net.createServer(socket => {
+  let date = makeDate();
+  socket.end(date + "\n");
+});
 
-const url1 = process.argv[2];
-const url2 = process.argv[3];
-const url3 = process.argv[4];
-let result = "";
+const portNumber = process.argv[2];
+server.listen(portNumber);
 
-http.get(url1, res1 => {
-  res1.setEncoding('utf8');
-  res1.on('data', data => result += data);
-  res1.on('end', () => {
-    console.log(result);
-    http.get(url2, res2 => {
-      res2.setEncoding('utf8');
-      result = "";
-      res2.on('data', data => result += data);
-      res2.on('end', () => {
-        console.log(result);
-        http.get(url3, res3 => {
-          res3.setEncoding('utf8');
-          result = "";
-          res3.on('data', data => result += data);
-          res3.on('end', () => {
-            console.log(result);
-          });
-        });
-      });
-    });
-  });
-}).on('error', err => console.error(err));
+function makeDate() {
+  let date = new Date;
+  let dateString = "";
+
+  dateString = dateString + date.getFullYear() + "-";
+  dateString = dateString + parseMonth(date) + "-";
+  dateString = dateString + parseDay(date) + " ";
+  dateString = dateString + parseHours(date) + ":";
+  dateString = dateString + parseMinutes(date);
+
+  return dateString;
+}
+
+function parseMonth(date) {
+  let month = date.getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+}
+
+function parseDay(date) {
+  let day = date.getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+}
+
+function parseHours(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    return `0${hours}`;
+  } else {
+    return hours;
+  }
+}
+
+function parseMinutes(date) {
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    return `0${minutes}`;
+  } else {
+    return minutes;
+  }
+}
+
+// const http = require('http');
+//
+// const url1 = process.argv[2];
+// const url2 = process.argv[3];
+// const url3 = process.argv[4];
+// let result = "";
+//
+// http.get(url1, res1 => {
+//   res1.setEncoding('utf8');
+//   res1.on('data', data => result += data);
+//   res1.on('end', () => {
+//     console.log(result);
+//     http.get(url2, res2 => {
+//       res2.setEncoding('utf8');
+//       result = "";
+//       res2.on('data', data => result += data);
+//       res2.on('end', () => {
+//         console.log(result);
+//         http.get(url3, res3 => {
+//           res3.setEncoding('utf8');
+//           result = "";
+//           res3.on('data', data => result += data);
+//           res3.on('end', () => {
+//             console.log(result);
+//           });
+//         });
+//       });
+//     });
+//   });
+// }).on('error', err => console.error(err));
 
 // const http = require('http');
 // http.get(process.argv[2], res => {
